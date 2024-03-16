a05f83b6df063f691b78dad922afe7abc514a6e5
Peter White <forbiddenforge64@gmail.com>
2024-03-15 20:59:24 -0500

    prepare model files

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -107,9 +107,15 @@ const corresponding = {
 let setupMode = false;
 
 export function Avatar(props) {
+  // Male Model
   const { nodes, materials, scene } = useGLTF(
-    "/models/64f1a714fe61576b46f27ca2.glb"
+    "/models/p.glb"
   );
+
+  // FEMALE Model
+  // const { nodes, materials, scene } = useGLTF(
+  // "/models/64f1a714fe61576b46f27ca2.glb"
+  // );
 
   const { message, onMessagePlayed, chat } = useChat();
 
@@ -130,7 +136,11 @@ export function Avatar(props) {
     audio.onended = onMessagePlayed;
   }, [message]);
 
-  const { animations } = useGLTF("/models/animations.glb");
+  // Male Model
+  const { animations } = useGLTF("/models/p_animations.glb");
+
+  // FEMALE Model
+  // const { animations } = useGLTF("/models/animations.glb");
 
   const group = useRef();
   const { actions, mixer } = useAnimations(animations, group);
@@ -309,36 +319,6 @@ export function Avatar(props) {
     <group {...props} dispose={null} ref={group}>
       <primitive object={nodes.Hips} />
       <skinnedMesh
-        name="Wolf3D_Body"
-        geometry={nodes.Wolf3D_Body.geometry}
-        material={materials.Wolf3D_Body}
-        skeleton={nodes.Wolf3D_Body.skeleton}
-      />
-      <skinnedMesh
-        name="Wolf3D_Outfit_Bottom"
-        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-        material={materials.Wolf3D_Outfit_Bottom}
-        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-      />
-      <skinnedMesh
-        name="Wolf3D_Outfit_Footwear"
-        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-        material={materials.Wolf3D_Outfit_Footwear}
-        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-      />
-      <skinnedMesh
-        name="Wolf3D_Outfit_Top"
-        geometry={nodes.Wolf3D_Outfit_Top.geometry}
-        material={materials.Wolf3D_Outfit_Top}
-        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-      />
-      <skinnedMesh
-        name="Wolf3D_Hair"
-        geometry={nodes.Wolf3D_Hair.geometry}
-        material={materials.Wolf3D_Hair}
-        skeleton={nodes.Wolf3D_Hair.skeleton}
-      />
-      <skinnedMesh
         name="EyeLeft"
         geometry={nodes.EyeLeft.geometry}
         material={materials.Wolf3D_Eye}
@@ -370,9 +350,103 @@ export function Avatar(props) {
         morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
         morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
       />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Hair.geometry}
+        material={materials.Wolf3D_Hair}
+        skeleton={nodes.Wolf3D_Hair.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Body.geometry}
+        material={materials.Wolf3D_Body}
+        skeleton={nodes.Wolf3D_Body.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+        material={materials.Wolf3D_Outfit_Bottom}
+        skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+        material={materials.Wolf3D_Outfit_Footwear}
+        skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+      />
+      <skinnedMesh
+        geometry={nodes.Wolf3D_Outfit_Top.geometry}
+        material={materials.Wolf3D_Outfit_Top}
+        skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+      />
     </group>
+    // FEMALE AVATAR BELOW
+    // <group {...props} dispose={null} ref={group}>
+    //   <primitive object={nodes.Hips} />
+    //   <skinnedMesh
+    //     name="Wolf3D_Body"
+    //     geometry={nodes.Wolf3D_Body.geometry}
+    //     material={materials.Wolf3D_Body}
+    //     skeleton={nodes.Wolf3D_Body.skeleton}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Outfit_Bottom"
+    //     geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
+    //     material={materials.Wolf3D_Outfit_Bottom}
+    //     skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Outfit_Footwear"
+    //     geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
+    //     material={materials.Wolf3D_Outfit_Footwear}
+    //     skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Outfit_Top"
+    //     geometry={nodes.Wolf3D_Outfit_Top.geometry}
+    //     material={materials.Wolf3D_Outfit_Top}
+    //     skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Hair"
+    //     geometry={nodes.Wolf3D_Hair.geometry}
+    //     material={materials.Wolf3D_Hair}
+    //     skeleton={nodes.Wolf3D_Hair.skeleton}
+    //   />
+    //   <skinnedMesh
+    //     name="EyeLeft"
+    //     geometry={nodes.EyeLeft.geometry}
+    //     material={materials.Wolf3D_Eye}
+    //     skeleton={nodes.EyeLeft.skeleton}
+    //     morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
+    //     morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+    //   />
+    //   <skinnedMesh
+    //     name="EyeRight"
+    //     geometry={nodes.EyeRight.geometry}
+    //     material={materials.Wolf3D_Eye}
+    //     skeleton={nodes.EyeRight.skeleton}
+    //     morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
+    //     morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Head"
+    //     geometry={nodes.Wolf3D_Head.geometry}
+    //     material={materials.Wolf3D_Skin}
+    //     skeleton={nodes.Wolf3D_Head.skeleton}
+    //     morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
+    //     morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+    //   />
+    //   <skinnedMesh
+    //     name="Wolf3D_Teeth"
+    //     geometry={nodes.Wolf3D_Teeth.geometry}
+    //     material={materials.Wolf3D_Teeth}
+    //     skeleton={nodes.Wolf3D_Teeth.skeleton}
+    //     morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
+    //     morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+    //   />
+    // </group>
   );
 }
-
+// Male Model
+useGLTF.preload("/models/p.glb");
+useGLTF.preload("/models/p_animations.glb");
+// Female Model
 useGLTF.preload("/models/64f1a714fe61576b46f27ca2.glb");
 useGLTF.preload("/models/animations.glb");
